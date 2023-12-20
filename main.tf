2180236533f71263f901f1a72aead5693d94ebd4
Jeff Chao <jeffchao@me.com>
2023-12-20 14:48:39 -0800

    Use new references API, update TF provider

--- main.tf
+++ main.tf
@@ -1,32 +1,11 @@
-terraform {
-  backend "http" {
-    address        = "https://api.abbey.io/terraform-http-backend"
-    lock_address   = "https://api.abbey.io/terraform-http-backend/lock"
-    unlock_address = "https://api.abbey.io/terraform-http-backend/unlock"
-    lock_method    = "POST"
-    unlock_method  = "POST"
-  }
+locals {
+  account_name = ""
+  repo_name = ""
 
-  required_providers {
-    abbey = {
-      source = "abbeylabs/abbey"
-      version = "0.2.4"
-    }
+  project_path = "github://${local.account_name}/${local.repo_name}"
+  policies_path = "${local.project_path}/policies"
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-}
-
-provider "abbey" {
-  # Configuration options
-  bearer_auth = var.abbey_token
-}
-
-provider "aws" {
-  region = "us-west-2" #CHANGEME
+  aws_account = ""
 }
 
 resource "abbey_grant_kit" "IAM_membership" {
@@ -48,30 +27,16 @@ resource "abbey_grant_kit" "IAM_membership" {
   }
 
   output = {
-    # Replace with your own path pointing to where you want your access changes to manifest.
-    # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://replace-me-with-organization/replace-me-with-repo/access.tf" #CHANGEME
+    location = "${local.project_path}/access.tf"
     append = <<-EOT
-      resource "aws_iam_user_group_membership" "user_{{ .data.system.abbey.identities.aws_iam.name }}_group_${data.aws_iam_group.group1.group_name}" {
-        user = "{{ .data.system.abbey.identities.aws_iam.name }}"
-        groups = ["${data.aws_iam_group.group1.group_name}"]
+      resource "aws_iam_user_group_membership" "user_{{ .user.aws_iam.id }}_group_${data.aws_iam_group.group1.group_name}" {
+        user = "{{ .user.aws_iam.${local.aws_account}.id }}"
+        groups = [data.aws_iam_group.group1.group_name]
       }
     EOT
   }
 }
 
-resource "abbey_identity" "user_1" {
-  abbey_account = "replace-me@example.com"
-  source = "aws_iam"
-  metadata = jsonencode(
-    {
-      name = "ReplaceWithAWSIamName" #CHANGEME
-    }
-  )
-}
-
 data "aws_iam_group" "group1" {
   group_name = "group1"
 }
-
-
